0af47b9295c976b3db3e96d7709ae8ba03d0c5e7
femmytedrey <femmytedrey@gmail.com>
2023-12-15 00:35:24 +0100

    initializing

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Review from './Review';
+import { FaSquareGithub } from "react-icons/fa6";
+function App() {
+  return <h2>
+    reviews project setup
+    <FaSquareGithub />
+  </h2>;
+}
+
+export default App;
