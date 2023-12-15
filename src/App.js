a446ed721c22ef8bbb6a2cfd5725c946c1f2e01d
femmytedrey <femmytedrey@gmail.com>
2023-12-15 01:25:59 +0100

    review component rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Review from './Review';
-import { FaSquareGithub } from "react-icons/fa6";
 function App() {
-  return <h2>
-    reviews project setup
-    <FaSquareGithub />
-  </h2>;
+  return <main>
+    <section className='container'>
+      <div className='title'>
+        <h2>our reviews</h2>
+        <div className='underline'></div>
+      </div>
+      <Review />
+    </section>
+  </main>;
 }
 
 export default App;
